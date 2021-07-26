58d3ef428bf48d8269a3afe15222ade70bd1f0e2
Joyce <yeechiajoyce@gmail.com>
2021-07-26 16:38:00 +0800

    其他｜移除未用dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './components/App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
-    composeEnhancers(applyMiddleware(thunk)),
+    composeEnhancers(applyMiddleware()),
 );
 
 ReactDOM.render(
